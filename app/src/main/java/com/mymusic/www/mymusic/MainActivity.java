4682e76001973eec79151c71b72f4ef494798e2c
jemshid123 <jemshidmh@gmail.com>
2017-08-01 18:23:55 +0530

    signup

--- app/src/main/java/com/mymusic/www/mymusic/MainActivity.java
+++ app/src/main/java/com/mymusic/www/mymusic/MainActivity.java
@@ -2,8 +2,11 @@ package com.mymusic.www.mymusic;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -19,28 +22,38 @@ import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
+AlertDialog.Builder build;
 
     Button google,fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        build=new AlertDialog.Builder(MainActivity.this);
+        google = (Button) findViewById(R.id.google);
+        fb = (Button) findViewById(R.id.fb);
+        build.setTitle("Couldn't connect");
+        build.setMessage("No Internet connection");
+        build.setIcon(R.mipmap.ic_launcher);
+        build.setCancelable(false);
+        build.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
-        final String[] per={android.Manifest.permission.GET_ACCOUNTS, android.Manifest.permission.GET_ACCOUNTS_PRIVILEGED};
-
-       google=(Button)findViewById(R.id.google);
-        fb=(Button)findViewById(R.id.fb);
-
-
+            }
+        });
+       final AlertDialog alert=build.create();
         /** google signup */
         google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                ActivityCompat.requestPermissions(MainActivity.this,per,1);
-                if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.GET_ACCOUNTS)== PackageManager.PERMISSION_GRANTED) {
-                    try {
+                try {
+                    if(isNetworkAvailable(getBaseContext()))
+                    {
+                    if (permission()) {
                         Pattern emailPattern = Patterns.EMAIL_ADDRESS;
                         Account[] accounts = AccountManager.get(getBaseContext()).getAccounts();
                         for (Account account : accounts) {
@@ -50,10 +63,16 @@ public class MainActivity extends AppCompatActivity {
 
                             }
                         }
-                    }catch (Exception e){e.printStackTrace();}
+                    }
+                    }else
+                    {
+
+                        alert.show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
-
 
 
             }
@@ -62,11 +81,11 @@ public class MainActivity extends AppCompatActivity {
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                if(isNetworkAvailable(getBaseContext()))
+                {
+                if (permission()) {
 
-
-                ActivityCompat.requestPermissions(MainActivity.this,per,1);
-                if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.GET_ACCOUNTS)== PackageManager.PERMISSION_GRANTED) {
-                    try {
                         Pattern emailPattern = Patterns.EMAIL_ADDRESS;
                         Account[] accounts = AccountManager.get(getBaseContext()).getAccounts();
                         for (Account account : accounts) {
@@ -76,17 +95,35 @@ public class MainActivity extends AppCompatActivity {
 
                             }
                         }
-                    }catch (Exception e){e.printStackTrace();}
+
                 }
+                }else
+                {
 
-
-
+                    alert.show();
+                }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
             }
         });
+    }
+public Boolean permission(){
+        final String[] per={android.Manifest.permission.GET_ACCOUNTS, android.Manifest.permission.GET_ACCOUNTS_PRIVILEGED, android.Manifest.permission.INTERNET, android.Manifest.permission.ACCESS_NETWORK_STATE};
+        ActivityCompat.requestPermissions(MainActivity.this,per,1);
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.GET_ACCOUNTS)== PackageManager.PERMISSION_GRANTED) {
+return true;
+        }
 
+return false;
+    }
 
-
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
